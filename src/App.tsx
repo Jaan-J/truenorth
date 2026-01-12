6999d4110c8efdfa70b2f8e57c2f8cef684350e3
Jaan-J <jaan.develop@gmail.com>
2026-01-12 18:25:33 -0500

    add services page

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/services" element={<Services />} />
-          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/services" element={<Services />} />
+          {/* <Route path="/pricing" element={<Pricing />} />
           <Route path="/book" element={<BookCall />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
